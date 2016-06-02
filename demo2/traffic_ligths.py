50389e066a187a615ccad515fe71ca76f82532b5
Matti Ahinko <matti.ahinko@gmail.com>
2016-06-02 19:25:03 +0300

    Add function to switch traffic lights green to red

--- demo2/traffic_ligths.py
+++ demo2/traffic_ligths.py
@@ -10,6 +10,8 @@ LED_PEDESTRIAN_GREEN = 26
 
 BUTTON = 4
 
+LIGHT_DELAY = 1
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_CAR_RED, GPIO.OUT)
 GPIO.setup(LED_CAR_YELLOW, GPIO.OUT)
@@ -18,3 +20,14 @@ GPIO.setup(LED_PEDESTRIAN_RED, GPIO.OUT)
 GPIO.setup(LED_PEDESTRIAN_GREEN, GPIO.OUT)
 
 GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def switch_to_car_red():
+  GPIO.output(LED_CAR_GREEN, False)
+  GPIO.output(LED_CAR_YELLOW, True)
+  time.sleep(LIGHT_DELAY)
+  GPIO.output(LED_CAR_YELLOW, False)
+  GPIO.output(LED_CAR_RED, True)
+  time.sleep(LIGHT_DELAY)
+  GPIO.output(LED_PEDESTRIAN_RED, False)
+  GPIO.output(LED_PEDESTRIAN_GREEN, True)
+
